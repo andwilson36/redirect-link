5349409f8f1cbd4fe9b8184b0d0a3634a401b560
andwilson36 <wilsonandrew57@gmail.com>
2023-08-07 18:39:17 -0500

    Dynamically changing title

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 const urlParams = new URLSearchParams(window.location.search);
+const titleReg = /(\b[a-z](?!\s))/g;
+const title = urlParams
+                .get("sitename")
+                .replace(/-/g, ' ')
+                .replace(titleReg, function(x){return x.toUpperCase();});
+
+document
+    .title(`${title}'s Store`);
 
 document
     .querySelector('meta[http-equiv="refresh"]')
